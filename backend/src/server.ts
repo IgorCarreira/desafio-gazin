0ae6343f4649970eda17aa387c93e1bd42d1065d
igor.picolo <igor.picolo@eurekalabs.com.br>
2024-11-09 03:00:25 -0300

    feat: registra services de nivel e desenvolvedor

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,24 @@
 import fastify from "fastify";
+import { createDeveloper } from "./http/routes/developer/create-developer";
+import { deleteDeveloper } from "./http/routes/developer/delete-developer";
+import { listDevelopers } from "./http/routes/developer/list-developers";
+import { updateDeveloper } from "./http/routes/developer/patch-developer";
+import { createLevel } from "./http/routes/level/create-level";
+import { deleteLevel } from "./http/routes/level/delete-level";
+import { listLevels } from "./http/routes/level/list-levels";
+import { updateLevel } from "./http/routes/level/patch-level";
 
 const app = fastify();
+
+app.register(createLevel);
+app.register(updateLevel);
+app.register(deleteLevel);
+app.register(listLevels);
+
+app.register(createDeveloper);
+app.register(updateDeveloper);
+app.register(deleteDeveloper);
+app.register(listDevelopers);
 
 app.listen({ port: 3030 }).then(() => {
   console.log("HTTP server running");
