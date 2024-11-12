bd7db8c5f610b38a000b2e6e10c2724624f8e101
igor.picolo <igor.picolo@eurekalabs.com.br>
2024-11-11 21:34:54 -0300

    feat: adiciona tipagens

--- frontend/src/types/developer.ts
+++ frontend/src/types/developer.ts
@@ -9,3 +9,18 @@ export interface Developer {
   hobby: string;
   nivel: Level;
 }
+
+export interface DeveloperBodyRequest {
+  nome?: string;
+  sexo?: string;
+  data_nascimento?: string;
+  idade?: number;
+  hobby?: string;
+  nivel_id?: number;
+}
+
+export interface DeveloperFilterRequest {
+  id?: number | null;
+  nome?: string | null;
+  nivel_id?: number | null;
+}
