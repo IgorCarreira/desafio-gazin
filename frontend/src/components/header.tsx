e740d85069f06a07a453e6a5859735f40c6489fa
igor.picolo <igor.picolo@eurekalabs.com.br>
2024-11-13 15:02:58 -0300

    feat: adiciona link para a documentação da API

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,6 +1,7 @@
 import { CodeXml, Signal, User } from "lucide-react";
-import NavLink from "./nav-link";
+import { Link } from "react-router-dom";
 import { ModeToggle } from "./mode-toggle";
+import NavLink from "./nav-link";
 
 export const Header = () => {
   return (
@@ -20,7 +21,14 @@ export const Header = () => {
           </NavLink>
         </nav>
 
-        <div className="ml-auto flex items-center gap-2">
+        <div className="ml-auto flex items-center gap-8">
+          <Link
+            to={`${import.meta.env.VITE_API_BASE_URL}/docs`}
+            target="_blank"
+            className="underline underline-offset-2"
+          >
+            Documentação da API
+          </Link>
           <ModeToggle />
         </div>
       </div>
